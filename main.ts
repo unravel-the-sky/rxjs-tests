25bab819137e5a0264dd56fd90a55168555cbd41
unravel-the-sky <sekdemir@gmail.com>
2017-04-17 02:08:56 +0300

    update; this rxjs shit is KEWL;

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-alert("weee!");
+import {Observable} from "rxjs";
+
+let numbers = [1,2,3];
+let source = Observable.create(observer => {
+    for(let n of numbers){
+        observer.next(n);
+    }
+
+    observer.complete();
+})
+
+source.subscribe(
+    val => console.log(`value: ${val}`),
+    e => console.log("errrrorr"),
+    () => console.log("completed")
+)
